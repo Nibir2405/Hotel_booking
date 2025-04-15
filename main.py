3ca2c3bec4a37fc8fa5f60b1de1212cdd42996b3
Nibir2405 <nibirislam56@gmail.com>
2025-04-16 03:09:05 +0600

    Implement hotel booking functionality and update planning document

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+import pandas
+
+df = pandas.read_csv("hotels.csv")
+
+
+
 class Hotel:
+    def __init__(self,id):
+        pass
+
     def book(self):
         pass
 
+    def available(self):
+        pass
+
 class ReservationTicket:
-    def get(self):
+    def __init__(self, customer_name, date, hotel_match):
+        pass
+    def generate(self):
         pass
 
 
 class Email:
     def send(self):
         pass
+
+
+print(df)
+id = input("Enter the hotel id of the selected hotel: ")
+hotel = Hotel(id)
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_date = input("Enter the date you want to stay in the hotel: ")
+    reservation_ticket = ReservationTicket(name, reservation_date, hotel)
