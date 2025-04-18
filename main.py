116a670e3cda8a3dc885d399921de7617752588c
Nibir2405 <nibirislam56@gmail.com>
2025-04-19 00:53:47 +0600

    Refactor Hotel and ReservationTicket classes to improve initialization and booking message generation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ df = pandas.read_csv("hotels.csv", dtype={"id": str})
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"]==self.hotel_id, "name"].squeeze()
 
     def book(self):
         """By booking a hotels,changes the hotel availability"""
@@ -15,7 +16,7 @@ class Hotel:
 
     def available(self):
         """Checks if the hotel id is available"""
-        availability = df.loc[df["id"]==self.hotel_id, "available"]
+        availability = df.loc[df["id"]==self.hotel_id, "available"].squeeze()
         if availability.iloc[0]== "yes":
             return True
         else:
@@ -24,10 +25,18 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self, customer_name, date, hotel_match):
-        pass
+        self.customer_name = customer_name
+        self.date = date
+        self.hotel = hotel_match
     def generate(self):
-        pass
-
+        content = f"""
+        Thank you for your reservation.
+        Here is your booking data:
+        Name: {self.customer_name}
+        Hotel: {self.hotel.name}
+        Check in: {self.date}
+        """
+        return content
 
 class Email:
     def send(self):
@@ -40,5 +49,9 @@ hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
-    reservation_date = input("Enter the date you want to stay in the hotel: ")
-    reservation_ticket = ReservationTicket(name, reservation_date, hotel)
+    reservation_date = input("Enter the date you want to check in: ")
+    reservation_ticket = ReservationTicket(hotel_match=hotel,customer_name=name,date=reservation_date)
+    print(reservation_ticket.generate())
+
+else:
+    print("Hotel is not available")
