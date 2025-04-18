89440b13babf8a7e73fdf844abfbb6d4938ef25a
Nibir2405 <nibirislam56@gmail.com>
2025-04-19 00:29:56 +0600

    Update hotel availability logic and enhance planning document

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pandas
 
-df = pandas.read_csv("hotels.csv")
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
 
 
 class Hotel:
-    def __init__(self,id):
-        pass
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        """By booking a hotels,changes the hotel availability"""
+        df.loc[df["id"]==self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        """Checks if the hotel id is available"""
+        availability = df.loc[df["id"]==self.hotel_id, "available"]
+        if availability.iloc[0]== "yes":
+            return True
+        else:
+            return False
+        
 
 class ReservationTicket:
     def __init__(self, customer_name, date, hotel_match):
@@ -27,8 +35,8 @@ class Email:
 
 
 print(df)
-id = input("Enter the hotel id of the selected hotel: ")
-hotel = Hotel(id)
+hotel_ID = input("Enter the hotel id of the selected hotel: ")
+hotel = Hotel(hotel_ID)
 if hotel.available():
     hotel.book()
     name = input("Enter your name: ")
